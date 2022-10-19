85e71ea369bfa53d1d8a23f343aa8e028a8811fd
anthonyponplex <mayakianthony@yahoo.com>
2022-10-19 16:41:30 +0100

    UI design completed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 20px;
+  ${mobile({ padding: "10px" })}
 `;
 
 const Title = styled.h1`
@@ -32,7 +34,9 @@ const TopButton = styled.button`
   color: ${(props) => props.type === "filled" && "white"};
 `;
 
-const TopTexts = styled.div``;
+const TopTexts = styled.div`
+  ${mobile({ display: "none" })}
+`;
 const TopText = styled.span`
   text-decoration: underline;
   cursor: pointer;
@@ -42,6 +46,7 @@ const TopText = styled.span`
 const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const Info = styled.div`
@@ -51,6 +56,7 @@ const Info = styled.div`
 const Product = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const ProductDetail = styled.div`
@@ -99,11 +105,13 @@ const ProductAmountContainer = styled.div`
 const ProductAmount = styled.div`
   font-size: 24px;
   margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
 `;
 
 const ProductPrice = styled.div`
   font-size: 30px;
   font-weight: 200;
+  ${mobile({ marginBottom: "20px" })}
 `;
 
 const Hr = styled.hr`
@@ -162,15 +170,15 @@ const Cart = () => {
           <Info>
             <Product>
               <ProductDetail>
-                <Image src="https://pagesix.com/wp-content/uploads/sites/3/2022/01/new-blanace.png" />
+                <Image src="https://www.pngarts.com/files/6/Jeans-Transparent-Image.png" />
                 <Details>
                   <ProductName>
-                    <b>Product:</b>JESSIE THUNDER SHOES
+                    <b>Product:</b>MEN JEANS
                   </ProductName>
                   <ProductId>
                     <b>ID:</b>93813718293
                   </ProductId>
-                  <ProductColor color="black" />
+                  <ProductColor color="Blue" />
                   <ProductSize>
                     <b>Size:</b>37.5
                   </ProductSize>
@@ -188,15 +196,15 @@ const Cart = () => {
             <Hr />
             <Product>
               <ProductDetail>
-                <Image src="https://www.pngarts.com/files/12/Plain-Black-T-Shirt-PNG-Transparent-Image.png" />
+                <Image src="https://www.pngarts.com/files/9/Black-Hoodie-Front-PNG-Clipart-Background.png" />
                 <Details>
                   <ProductName>
-                    <b>Product:</b>PLAIN BLACK T-SHIRT
+                    <b>Product:</b>BLACK HOODIE
                   </ProductName>
                   <ProductId>
                     <b>ID:</b>938137182456
                   </ProductId>
-                  <ProductColor color="black" />
+                  <ProductColor color="Black" />
                   <ProductSize>
                     <b>Size:</b>M
                   </ProductSize>
